e74686f3e04bba12583400aee3bbc9f522d40888
depstor <depstor90@gmail.com>
2020-11-09 23:19:37 +0300

    gr

--- src/component/GroupTags.tsx
+++ src/component/GroupTags.tsx
@@ -1,24 +1,16 @@
-import React, { useState} from 'react'
+import React, { useState } from 'react'
 import { ITodo } from '../interfaces'
 import { GetGroupped } from '../component/Selectors/GetGroupped'
- 
+
 interface TodosProps {
     todos: ITodo[]
 }
 
 export const GroupTags: React.FC<TodosProps> = ({ todos }) => {
     const [change, setChange] = useState<string>('Группировать')
-<<<<<<< HEAD
-    const [grouped, setGrouped] = useState<any>({})
-=======
     const [grouped, setGrouped] = useState<any>()
->>>>>>> d87863060f66af19dfd9c055b40240fe017eb90b
 
 
-<<<<<<< HEAD
-        if (change === 'Группировать') {
-            console.log(grouped)
-=======
 
     const groupTag = (event: React.MouseEvent) => {
 
@@ -26,16 +18,15 @@ export const GroupTags: React.FC<TodosProps> = ({ todos }) => {
             const arrayGroup = todos.reduce((array: any, arg: any) => {
                 array[arg.tag] = array[arg.tag] || [];
                 array[arg.tag].push(arg)
-                return  array
+                return array
             }, {})
             if (grouped === undefined) {
                 console.log('error')
             } else {
                 // console.log(Object.values(grouped))
-                console.log(grouped.va)
+                console.log(grouped)
             }
             setGrouped(arrayGroup)
->>>>>>> d87863060f66af19dfd9c055b40240fe017eb90b
             setChange('Разгруппировать')
         } else {
             setChange('Группировать')
@@ -45,7 +36,7 @@ export const GroupTags: React.FC<TodosProps> = ({ todos }) => {
     }
 
     return <>
-    <GetGroupped grouped={grouped}/>
+        <GetGroupped grouped={grouped} />
         <input
             type="button"
             onClick={groupTag}
